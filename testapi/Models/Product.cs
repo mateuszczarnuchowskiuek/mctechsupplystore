55a8f21c571cdcf8149593b43b772aa74a406edb
s-A301-25 <s-A301-25@K-A301-25>
2025-03-09 10:17:14 +0100

    Dodaj pliki projektów.

---
+++ testapi/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace testapi.Models
+{
+    public class Product
+    {
+        public int Id;
+        public string name;
+        public string ean;
+        public decimal price;
+        public int stock;
+        public string sku;
+        public Category category;
+        public bool deleted;
+        public DateTime created_at;
+        public 
+    }
+}
