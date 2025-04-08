631fd53005c88825a16df4d003b46893b93d4e5f
mateuszczarnuchowskiuek <234189@student.uek.krakow.pl>
2025-04-09 01:25:45 +0200

    Finished tests. Better test data and did card service.

--- EShop.Application/Services/CardService.cs
+++ EShop.Application/Services/CardService.cs
@@ -7,7 +7,11 @@ public class CardService
     public bool ValidateCard(string cardNumber)
     {
         cardNumber = cardNumber.Replace(" ", "");
+        cardNumber = cardNumber.Replace("-", "");
         if (!cardNumber.All(char.IsDigit))
+            return false;
+
+        if (cardNumber.Length<13 || cardNumber.Length>19)
             return false;
 
         int sum = 0;
@@ -29,30 +33,5 @@ public class CardService
         }
 
         return (sum % 10 == 0);
-    }
-
-    public string GetCardType(string cardNumber)
-    {
-        cardNumber = cardNumber.Replace(" ", "").Replace("-", "");
-
-        if (Regex.IsMatch(cardNumber, @"^4(\d{12}|\d{15}|\d{18})$"))
-            return "Visa";
-        else if (Regex.IsMatch(cardNumber, @"^(5[1-5]\d{14}|2(2[2-9][1-9]|2[3-9]\d{2}|[3-6]\d{3}|7([01]\d{2}|20\d))\d{10})$"))
-            return "MasterCard";
-
-        if (Regex.IsMatch(cardNumber, @"^3[47]\d{13}$"))
-            return "American Express";
-
-        if (Regex.IsMatch(cardNumber, @"^(6011\d{12}|65\d{14}|64[4-9]\d{13}|622(1[2-9][6-9]|[2-8]\d{2}|9([01]\d|2[0-5]))\d{10})$"))
-            return "Discover";
-
-        if (Regex.IsMatch(cardNumber, @"^(352[89]|35[3-8]\d)\d{12}$"))
-            return "JCB";
-
-        if (Regex.IsMatch(cardNumber, @"^3(0[0-5]|[68]\d)\d{11}$"))
-            return "Diners Club";
-
-        if (Regex.IsMatch(cardNumber, @"^(50|5[6-9]|6\d)\d{10,17}$"))
-            return "Maestro";
     }
 }
--- Eshop.Application.Tests/Services/CardServiceTests.cs
+++ Eshop.Application.Tests/Services/CardServiceTests.cs
@@ -11,56 +11,78 @@ public class CardServiceTests
     [Fact]
     public void ValidateCard_CardNumberHas12Digits_ExpectedFalse()
     {
-        string input = "12-345 67890  12";
+        string input = "230914554590";
         var service = new CardService();
         Assert.False(service.ValidateCard(input));
     }
     [Fact]
     public void ValidateCard_CardNumberHas20Digits_ExpectedFalse()
     {
-        string input = "12-345 67890  1234567890";
+        string input = "26081981298536520573";
         var service = new CardService();
         Assert.False(service.ValidateCard(input));
     }
     [Fact]
     public void ValidateCard_CardNumberHas13Digits_ExpectedTrue()
     {
-        string input = "12-345 67890  123";
+        string input = "0091745169226";
         var service = new CardService();
         Assert.True(service.ValidateCard(input));
     }
     [Fact]
     public void ValidateCard_CardNumberHas15Digits_ExpectedTrue()
     {
-        string input = "12-345 67890  12345";
+        string input = "841772999127548";
         var service = new CardService();
         Assert.True(service.ValidateCard(input));
     }
     [Fact]
     public void ValidateCard_CardNumberHas19Digits_ExpectedTrue()
     {
-        string input = "12-345 67890  123456789";
+        string input = "5252074388656952720";
         var service = new CardService();
         Assert.True(service.ValidateCard(input));
     }
     [Fact]
     public void ValidateCard_CardNumberHas15DigitsWithSpaces_ExpectedTrue()
     {
-        string input = "  3497 7965  8312 797  ";
+        string input = "  60260 219  49090  86  ";
         var service = new CardService();
         Assert.True(service.ValidateCard(input));
     }
     [Fact]
     public void ValidateCard_CardNumberHas15DigitsWithDashes_ExpectedTrue()
     {
-        string input = "--3497-7965--8312-797--";
+        string input = "--6026-0219--4909086---";
         var service = new CardService();
         Assert.True(service.ValidateCard(input));
     }
     [Fact]
     public void ValidateCard_CardNumberHas15DigitsWithSpacesAndDashes_ExpectedTrue()
     {
-        string input = "  3497-7965--  8312 797--";
+        string input = "  602  6021 949-090--86--";
+        var service = new CardService();
+        Assert.True(service.ValidateCard(input));
+    }
+    [Fact]
+    public void ValidateCard_CardNumberContainsInvalidCharacters_ExpectedFalse()
+    {
+        string input = "v4344Oh4397938910p";
+        var service = new CardService();
+        Assert.False(service.ValidateCard(input));
+    }
+    [Theory]
+    [InlineData("3497 7965 8312 797")]
+    [InlineData("345-470-784-783-010")]
+    [InlineData("378523393817437")]
+    [InlineData("4024-0071-6540-1778")]
+    [InlineData("4532 2080 2150 4434")]
+    [InlineData("4532289052809181")]
+    [InlineData("5530016454538418")]
+    [InlineData("5551561443896215")]
+    [InlineData("5131208517986691")]
+    public void ValidateCard_ValidCardNumber(string input)
+    {
         var service = new CardService();
         Assert.True(service.ValidateCard(input));
     }
