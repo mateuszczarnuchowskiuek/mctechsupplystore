0d6296b38523da3522e3bea97d6eec559f767155
234189@student.uek.krakow.pl <234189@student.uek.krakow.pl>
2025-04-26 19:59:58 +0200

    Stwórz plik Repository i zaimplementuj w nim brakujące metody

---
+++ EShop.Domain/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+using EShop.Domain.Models;
+
+namespace EShop.Domain.Repositories;
+
+public interface IRepository
+{
+    #region Product
+    Task<Product> GetProductAsync(int id);
+    Task<Product> AddProductAsync(Product product);
+    Task<Product> UpdateProductAsync(Product user);
+    Task<List<Product>> GetAllProductAsync();
+    #endregion
+}
