743a77ed42521b4f1eafb15944b060c5d51ff48e
234189@student.uek.krakow.pl <234189@student.uek.krakow.pl>
2025-04-26 03:14:22 +0200

    Przekształć model Product na encję bazodanową.

--- EShop.Domain/Repositories/DataContext.cs
+++ EShop.Domain/Repositories/DataContext.cs
@@ -7,6 +7,7 @@ public class DataContext : DbContext
 {
     public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
+    //
     public DbSet<Product> Products { get; set; }
     public DbSet<Category> Categories { get; set; }
 }
