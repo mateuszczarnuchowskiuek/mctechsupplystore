90ef6d191a489ab876778687dec9773ce386a2b9
234189@student.uek.krakow.pl <234189@student.uek.krakow.pl>
2025-04-26 03:11:51 +0200

    Podłącz do odpowiedniego projektu Entity Framework (na potrzeby zajęć w pracowni podłącz Entity Framework In Memory). Klasę zawierającą kontekst bazy danych nazwij DataContext i umieść w projekcie EShop.Domain w katalogu Repositories.

--- testapi/Program.cs
+++ testapi/Program.cs
@@ -1,4 +1,6 @@
 using EShop.Application.Services;
+using EShop.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(options => options.UseInMemoryDatabase("TestDb"), ServiceLifetime.Transient);
 
 var app = builder.Build();
 
