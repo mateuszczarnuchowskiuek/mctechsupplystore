bd3775491f96106ac6faddbf91992c055feab581
234189@student.uek.krakow.pl <234189@student.uek.krakow.pl>
2025-06-22 03:45:44 +0200

    migration happened

---
+++ EShop.Domain/Migrations/20250622014256_newmig2.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EShop.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class newmig2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Categories_categoryid",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "stock",
+                table: "Products",
+                newName: "Stock");
+
+            migrationBuilder.RenameColumn(
+                name: "sku",
+                table: "Products",
+                newName: "Sku");
+
+            migrationBuilder.RenameColumn(
+                name: "price",
+                table: "Products",
+                newName: "Price");
+
+            migrationBuilder.RenameColumn(
+                name: "name",
+                table: "Products",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "ean",
+                table: "Products",
+                newName: "Ean");
+
+            migrationBuilder.RenameColumn(
+                name: "categoryid",
+                table: "Products",
+                newName: "CategoryId");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Products",
+                newName: "Id");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Products_categoryid",
+                table: "Products",
+                newName: "IX_Products_CategoryId");
+
+            migrationBuilder.RenameColumn(
+                name: "name",
+                table: "Categories",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Categories",
+                newName: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "Stock",
+                table: "Products",
+                newName: "stock");
+
+            migrationBuilder.RenameColumn(
+                name: "Sku",
+                table: "Products",
+                newName: "sku");
+
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "Products",
+                newName: "price");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Products",
+                newName: "name");
+
+            migrationBuilder.RenameColumn(
+                name: "Ean",
+                table: "Products",
+                newName: "ean");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoryId",
+                table: "Products",
+                newName: "categoryid");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Products",
+                newName: "id");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                newName: "IX_Products_categoryid");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Categories",
+                newName: "name");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Categories",
+                newName: "id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Categories_categoryid",
+                table: "Products",
+                column: "categoryid",
+                principalTable: "Categories",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- EShop.Domain/Migrations/20250622014457_newmig3.Designer.cs
+++ EShop.Domain/Migrations/20250622014457_newmig3.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EShop.Domain.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EShop.Domain.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20250622014457_newmig3")]
+    partial class newmig3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.6");
